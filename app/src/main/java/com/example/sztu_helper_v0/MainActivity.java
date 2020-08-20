f17b852f6ae3f62bb04433a38583d7108571097c
Chrystal <976583293@qq.com>
2020-08-21 03:47:34 +0800

    *just finished the UI of login page and going to complete the program logic inside it.

---
+++ app/src/main/java/com/example/sztu_helper_v0/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.sztu_helper_v0;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText et_account;
+    private EditText et_password;
+    private Button btn_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
